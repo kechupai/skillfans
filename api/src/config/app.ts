af37a7a5579c3596105a7e963ca6991f688add01
richasinhaa <richa.sinha0603@gmail.com>
2022-07-26 23:05:51 +0530

    config changes

--- api/src/config/app.ts
+++ api/src/config/app.ts
@@ -1,4 +1,4 @@
 export default {
-  port: process.env.HTTP_PORT || 8080,
+  port: process.env.HTTP_PORT || 80,
   baseUrl: process.env.BASE_URL || 'http://ec2-18-222-228-243.us-east-2.compute.amazonaws.com'// 'http://localhost:9000' //
 };
